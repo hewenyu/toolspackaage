33e69ebeaa9b2136199d95b33128909c5078964d
yueban <yuebanlaosiji@outlook.com>
2021-03-05 15:33:36 +0800

    切片常用数据处理

---
+++ format/slice/copy.go
@@ -0,0 +1,117 @@
+package slice
+
+/*
+see https://github.com/golang/go/wiki/SliceTricks
+append(s[:0:0], s...) 表示在一个空切片基础上插入s
+从数组或者切片派生切片（取子切片）有两以下两种方式
+s[low : high]       // 双下标形式
+s[low : high : max] // 三下标形式
+双下标实际是三下标的简写
+s[low : high] ==>s[low : high:cap(s)]
+以下切片等价
+s[0 : len(s)]
+s[: len(s)]
+s[0 :]
+s[:]
+s[0 : len(s) : cap(s)]
+s[: len(s) : cap(s)]
+*/
+//安全的复制切片
+func Copy(s []interface{}) []interface{} {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyString(s []string) []string {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyInt(s []int) []int {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyInt8(s []int8) []int8 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyInt16(s []int16) []int16 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyInt32(s []int32) []int32 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyInt64(s []int64) []int64 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUint(s []uint) []uint {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUint8(s []uint8) []uint8 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUint16(s []uint16) []uint16 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUint32(s []uint32) []uint32 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUint64(s []uint64) []uint64 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyUintptr(s []uintptr) []uintptr {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyFloat32(s []float32) []float32 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyFloat64(s []float64) []float64 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyRune(s []rune) []rune {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyByte(s []byte) []byte {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyComplex64(s []complex64) []complex64 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyComplex128(s []complex128) []complex128 {
+	return append(s[:0:0], s...)
+}
+
+//安全的复制切片
+func CopyBool(s []bool) []bool {
+	return append(s[:0:0], s...)
+}
---
+++ format/slice/reverse.go
@@ -0,0 +1,150 @@
+package slice
+
+//对切片进行倒序排列
+func Reverse(arr []interface{}) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseString(s []string) {
+	for from, to := 0, len(s)-1; from < to; from, to = from+1, to-1 {
+		s[from], s[to] = s[to], s[from]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseInt(arr []int) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseInt8(arr []int8) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseInt16(arr []int16) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseInt32(arr []int32) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseInt64(arr []int64) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUint(arr []uint) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUint8(arr []uint8) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUint16(arr []uint16) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUint32(arr []uint32) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUint64(arr []uint64) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseUintptr(arr []uintptr) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseFloat32(arr []float32) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseFloat64(arr []float64) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseRune(s []rune) {
+	for from, to := 0, len(s)-1; from < to; from, to = from+1, to-1 {
+		s[from], s[to] = s[to], s[from]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseByte(arr []byte) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseComplex64(arr []complex64) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseComplex128(arr []complex128) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+//对切片进行倒序排列
+func ReverseBool(arr []bool) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+}
+
+// /*
+// reverse 翻转专用,使用原地算法
+// */
+// func reverse(arr []int) {
+// 	for i := 0; i < len(arr)/2; i++ {
+// 		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+// 	}
+// }
