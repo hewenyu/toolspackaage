b33acde1028f15b72b04b691588353cbec682864
yueban <yuebanlaosiji@outlook.com>
2021-03-05 16:50:35 +0800

    切片排序

---
+++ format/slice/sort.go
@@ -0,0 +1,108 @@
+package slice
+
+import "sort"
+
+func SortString(s []string) {
+	sort.Strings(s)
+}
+
+//切片排序
+func SortInt(s []int) {
+	sort.Ints(s)
+}
+
+//切片排序
+func SortInt8(s []int8) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortInt16(s []int16) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortInt32(s []int32) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortInt64(s []int64) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUint(s []uint) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUint8(s []uint8) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUint16(s []uint16) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUint32(s []uint32) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUint64(s []uint64) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortUintptr(s []uintptr) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortFloat32(s []float32) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortFloat64(s []float64) {
+	sort.Float64s(s)
+}
+
+//切片排序
+func SortRune(s []rune) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
+
+//切片排序
+func SortByte(s []byte) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+}
