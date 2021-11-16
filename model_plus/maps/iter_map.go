63e7488b9628cd4359e01c7eb13c4f897aeb6149
yueban <yuebanlaosiji@outlook.com>
2021-11-16 14:06:34 +0800

    格式化数据

--- model_plus/maps/iter_map.go
+++ model_plus/maps/iter_map.go
@@ -12,7 +12,7 @@ import (
 func iterSlice(arr interface{}) (map[string]interface{}, error) {
 	v := reflect.ValueOf(arr)
 	ret := make(map[string]interface{})
-
+	
 	if v.Kind() != reflect.Slice {
 		return ret, errors.New("not match Slice")
 	}
@@ -20,78 +20,78 @@ func iterSlice(arr interface{}) (map[string]interface{}, error) {
 	for i := 0; i < l; i++ {
 		/* 将结果转换成str格式符合输出 */
 		strI := strconv.Itoa(i)
-
+		
 		ret[strI] = v.Index(i).Interface()
 	}
-
+	
 	return ret, nil
 }
 
-/* map 格式化 */
+// IterMap map 格式化
 func IterMap(arr map[string]interface{}) (map[string]interface{}, error) {
 	v := reflect.ValueOf(arr)
 	ret := make(map[string]interface{})
-
+	
 	if v.Kind() != reflect.Map {
 		return ret, errors.New("not match Map")
 	}
 	return arr, nil
 }
 
-/* map 平铺展开*/
+// flatMap 平铺展开
 func flatMap(arr map[string]interface{}) (map[string]interface{}, error) {
-
+	
 	v := reflect.ValueOf(arr)
-
+	
 	if v.Kind() != reflect.Map {
 		return arr, errors.New("not match Map")
 	}
-
+	
 	reMaps := make(map[string]interface{})
-
+	
 	for keys, values := range arr {
 		/* 判断里面value的类型 */
 		vValues := reflect.ValueOf(values)
-
+		
 		switch vValues.Kind() {
 		case reflect.Slice:
 			ret, _ := flatSlice(values)
-
+			
 			for miniK, miniV := range ret {
 				fixStr := keys + "." + miniK
 				reMaps[fixStr] = miniV
 			}
-
+		
 		case reflect.Map:
 			rec, _ := values.(map[string]interface{})
 			ret, _ := flatMap(rec)
-
+			
 			for miniK, miniV := range ret {
 				fixStr := keys + "." + miniK
 				reMaps[fixStr] = miniV
 			}
 		default:
 			reMaps[keys] = values
-
+			
 		}
-
+		
 	}
 	return reMaps, nil
-
+	
 }
 
 /* Slice 平铺展开*/
 func flatSlice(arr interface{}) (map[string]interface{}, error) {
-
+	
 	reMaps := make(map[string]interface{})
 	retRes, _ := iterSlice(arr)
-
+	
 	for keys, values := range retRes {
 		v := reflect.ValueOf(values)
 		switch v.Kind() {
 		case reflect.Slice:
 			ret, _ := flatSlice(values)
-
+			
 			for miniK, miniV := range ret {
 				fixStr := keys + "." + miniK
 				reMaps[fixStr] = miniV
@@ -99,55 +99,58 @@ func flatSlice(arr interface{}) (map[string]interface{}, error) {
 		case reflect.Map:
 			rec, _ := values.(map[string]interface{})
 			ret, _ := flatMap(rec)
-
+			
 			for miniK, miniV := range ret {
 				fixStr := keys + "." + miniK
 				reMaps[fixStr] = miniV
 			}
 		default:
 			reMaps[keys] = values
-
+			
 		}
 	}
 	return reMaps, nil
 }
 
-/* 将多维json 扁平化输出为一维 */
+// TryToGet 将多维json 扁平化输出为一维
 func TryToGet(arr interface{}) (map[string]interface{}, error) {
 	/* 获取 输入 的属性 */
 	v := reflect.ValueOf(arr)
-
+	
 	switch v.Kind() {
 	case reflect.Slice:
 		ret, err := flatSlice(arr)
-
+		
 		return ret, err
 	case reflect.Map:
 		/* 强制替换成 map */
 		rec, _ := arr.(map[string]interface{})
-
+		
 		ret, err := flatMap(rec)
-
+		
 		return ret, err
-
+	
 	default:
 		ret := make(map[string]interface{})
 		/* 既不属于 Slice 也不属于 map */
 		return ret, errors.New("not map or Slice")
-
+		
 	}
 }
 
-/* 将一维的json 输出成str */
+// KeyValuePairs 将一维的json 输出成str
 func KeyValuePairs(m map[string]interface{}) string {
 	b := new(bytes.Buffer)
 	for key, value := range m {
-		fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+		_, err := fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+		if err != nil {
+			return ""
+		}
 	}
 	return b.String()
 }
 
-/* 将切片结果输出 */
+// ToSlice 将切片结果输出
 func ToSlice(arr interface{}) []interface{} {
 	v := reflect.ValueOf(arr)
 	if v.Kind() != reflect.Slice {
