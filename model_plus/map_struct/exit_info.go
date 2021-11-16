f72e2a6bad57bc69f548f3eea2cf9ab93fe04ed2
yueban <yuebanlaosiji@outlook.com>
2021-11-16 13:58:17 +0800

    update curd

--- model_plus/map_struct/exit_info.go
+++ model_plus/map_struct/exit_info.go
@@ -8,50 +8,10 @@ type ExitInfo struct {
 	data map[string]struct{}
 }
 
-
-
-// IsExit 是否存在
-func (d *ExitInfo) IsExit(keys string) bool {
-	
-	d.mu.RLock()
-	
-	defer d.mu.RUnlock()
-	
-	_, ok := d.data[keys]
-	if ok {
-		return true
-	} else {
-		return false
-	}
-}
-
-
-
-// Del 删除
-func (d *ExitInfo) Del(keys string) {
-	
-	d.mu.Lock()
-	
-	defer d.mu.Unlock()
-	
-	for k, _ := range d.data {
-		if k != keys {
-			d.data[k] = struct{}{}
-		}
-	}
-}
-
-// ReName 更新
-func (d *ExitInfo) ReName(keys string, newKeys string) {
-	
-	d.mu.Lock()
-	
-	defer d.mu.Unlock()
-	
-	for k, _ := range d.data {
-		if k == keys {
-			d.data[newKeys] = struct{}{}
-		}
+// NewExitInfo 初始化
+func NewExitInfo() *ExitInfo {
+	return &ExitInfo{
+		data: make(map[string]struct{}),
 	}
 }
 
--- model_plus/map_struct/new_exit_info.go
+++
@@ -1,8 +0,0 @@
-package map_struct
-
-// NewExitInfo 初始化
-func NewExitInfo() *ExitInfo {
-	return &ExitInfo{
-		data: make(map[string]struct{}),
-	}
-}
