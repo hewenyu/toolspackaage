f65538ae8eb9895fda540ee97f0b83b3ddfc0b26
yueban <yuebanlaosiji@outlook.com>
2021-11-16 13:42:21 +0800

    something update

--- main.go
+++ main.go
@@ -5,18 +5,19 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/hewenyu/toolspackage/global_variable"
 	"os"
 	"strings"
-
+	
 	"github.com/gomodule/redigo/redis"
 )
 
 var (
-	REDIS_HOST string
-	REDIS_PORT string
-	REDIS_PASS string
-	RedisUrl   string
-	REDIS_DB   string
+	RedisHost string
+	RedisPort string
+	RedisPass string
+	RedisUrl  string
+	RedisDb   string
 	// err        error
 )
 
@@ -24,16 +25,21 @@ var (
 redisGet 从redis中获取数据
 */
 func redisGet(keys string) (result_str string, err error) {
-
+	
 	conn, err := redis.DialURL(RedisUrl)
-
+	
 	if err != nil {
 		return
 	}
-	defer conn.Close()
-
+	defer func(conn redis.Conn) {
+		err := conn.Close()
+		if err != nil {
+			global_variable.CLOG.Error(err.Error())
+		}
+	}(conn)
+	
 	result, err := conn.Do("GET", keys)
-
+	
 	if err != nil {
 		return "", err
 	}
@@ -52,66 +58,71 @@ redis_host redis 链接地址
 redis_port redis 链接端口
 redis_db redis 使用的数据库
 */
-func formatRedisUrl(redis_pwd, redis_host, redis_port, redis_db string) (format_url string) {
-
-	if redis_pwd == "" || redis_pwd == "nil" {
-		format_url = "redis://" + redis_host + ":" + redis_port + "/" + redis_db
+func formatRedisUrl(redisPwd, redisHost, redisPort, redisDb string) (formatUrl string) {
+	
+	if redisPwd == "" || redisPwd == "nil" {
+		formatUrl = "redis://" + redisHost + ":" + redisPort + "/" + redisDb
 	} else {
-		format_url = "redis://:" + redis_pwd + "@" + redis_host + ":" + redis_port + "/" + redis_db
+		formatUrl = "redis://:" + redisPwd + "@" + redisHost + ":" + redisPort + "/" + redisDb
 	}
 	return
-
+	
 }
 
 /*
 redisGet 往redis写入数据获取数据
 */
 func RedisSet(keys string, values string) (result bool, err error) {
-
+	
 	var Expire = 60
 	conn, err := redis.DialURL(RedisUrl)
-
+	
 	if err != nil {
 		return
 	}
-	defer conn.Close()
-
+	defer func(conn redis.Conn) {
+		err := conn.Close()
+		if err != nil {
+			global_variable.CLOG.Error(err.Error())
+		}
+	}(conn)
+	
 	_, err = conn.Do("SET", keys, values)
-
+	
 	if err != nil {
 		return false, err
 	}
 	_, err = conn.Do("Expire", keys, Expire)
-
+	
 	if err != nil {
 		return false, err
 	}
-
+	
 	return true, nil
-
+	
 }
 
 func main() {
 	// export REDIS_HOST="124.71.182.117" REDIS_PORT="6379"  REDIS_PASS="WGRnZnVYZW9sRW81VEwwTgo=" REDIS_DB="0"
-
+	
 	var keys = flag.String("k", "", "redis获取的key")
-
+	
 	flag.Parse()
-	REDIS_HOST = os.Getenv("REDIS_HOST")
-	REDIS_PORT = os.Getenv("REDIS_PORT")
-	REDIS_PASS = os.Getenv("REDIS_PASS")
-	REDIS_DB = os.Getenv("REDIS_DB")
-
-	sDec, _ := base64.StdEncoding.DecodeString(REDIS_PASS)
-
-	RedisUrl = formatRedisUrl(strings.Replace(string(sDec), "\n", "", -1), REDIS_HOST, REDIS_PORT, REDIS_DB)
-
+	RedisHost = os.Getenv("REDIS_HOST")
+	RedisPort = os.Getenv("REDIS_PORT")
+	RedisPass = os.Getenv("REDIS_PASS")
+	RedisDb = os.Getenv("REDIS_DB")
+	
+	sDec, _ := base64.StdEncoding.DecodeString(RedisPass)
+	
+	RedisUrl = formatRedisUrl(strings.Replace(string(sDec), "\n", "", -1), RedisHost, RedisPort, RedisDb)
+	
 	result, err := redisGet(*keys)
-
+	
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		fmt.Println(result)
 	}
-
+	
 }
